57f4cc40cbc62c075ca590cc0016256b57a5fda9
silvicardo <silvicardo@gmail.com>
2018-11-28 13:01:21 +0100

    project files and basic structure

---
+++ main.js
@@ -0,0 +1,12 @@
+// jQuery - quadratini - main.js
+//Creare una griglia formata da 8x8 quadratini tutti bianchi
+//15 di questi quadratini(scelti a caso all'inizio) se
+//cliccati diventeranno rossi, gli altri diventeranno verdi.
+//Extra: Sopra alla griglia deve esserci un contatore che conta
+//quanti rossi e quanti verdi sono stati scoperti
+
+
+//64 quadratini, estrarre le posizioni dei quadratini
+var posizioniRossi = [1,8,9,12,15,18,20,25,34,59,60,61,62,63,64];
+
+//codice per modificare attributo colore in base all'oggetto toccato
